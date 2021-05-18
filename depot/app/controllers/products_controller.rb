ad778df426b5cc9f7d85638c74491316705ee029
Swa Yong Shen <yongshenswa@gmail.com>
2021-05-18 15:21:29 +0800

    Change sidebar, add navbar

--- depot/app/controllers/products_controller.rb
+++ depot/app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:index, :show, :new, :edit]
   before_action :set_product, only: %i[ show edit update destroy ]
 
   # GET /products or /products.json
