c6cf6b9ffc85622582e364fd24b9a5755191c8b4
Swa Yong Shen <yongshenswa@gmail.com>
2021-05-19 13:49:22 +0800

    Style edit account page with Bootstrap

--- depot/app/models/user.rb
+++ depot/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :products
+  has_many :products, dependent: delete_all
   has_many :line_items, through: :products
   has_many :orders, through: :line_items
 
